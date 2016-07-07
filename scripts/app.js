37d03ef7c85bd1dad25f82841d9e00171f527370
Minghan Lin <cd>
2016-07-08 06:03:05 +0800

    first calculator update

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+
+
+
+$('#basic-calc').click(clickButton);
+
+function clickButton(){
+  var numOne = $('numOne');
+  var numTwo = $('numTwo');
+  if $('#basic-operation' > 'option value='+'') {
+    return var numOne + var numTwo;
+  } else if $('#basic-operation' > 'option value='-'') {
+    return var numOne - var numTwo;
+  } else if $('#basic-operation' > 'option value='*'') {
+    return var numOne * var numTwo;
+  } else $('#basic-operation' > 'option value='/'') {
+    return var numOne / var numTwo;
+  }
