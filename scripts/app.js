2c2e1cabc42808753307a914ef5ea5b2f534bb30
Minghan Lin <cd>
2016-07-08 14:41:31 +0800

    A rough way of doing

--- scripts/app.js
+++ scripts/app.js
@@ -27,11 +27,11 @@ $(document).ready(function() {
 
 
 
-function clickButton() {
+function basicButton() {
   var $numOne = $('#basic-num-1');
   var $numTwo = $('#basic-num-2');
-  var $clickButton = $('#basic-calc');
-$clickButton.click(function() {
+  var $basicButton = $('#basic-calc');
+$basicButton.click(function() {
     if($("#basic-operation option:selected").html() === 'plus') {
     var sum = parseInt($numOne.val()) + parseInt($numTwo.val());
     $('#basic-answer-alert').html("The Answer is " + sum);
@@ -49,38 +49,61 @@ $('#basic-answer-alert').html("The Answer is " + divided);
 });
 }
 
+basicButton();
 
-  clickButton();
-
-
-  /*$(document).ready(function() {
-​
-​
-​
-function basicCalc() {
-  var numone = $('#basic-num-1');
-  var numtwo = $('#basic-num-2');
-  var calcnum = $('#basic-calc');
-  calcnum.click(function() {
-    if($("#basic-operation option:selected").html() === 'plus') {
-      var sum = parseInt(numone.val()) + parseInt(numtwo.val());
-      $('#basic-answer-alert').html("The Answer is " + sum);
-    }
-    else if($("#basic-operation option:selected").html() === 'minus') {
-      var remainder = parseInt(numone.val()) - parseInt(numtwo.val());
-      $('#basic-answer-alert').html("The Answer is " + remainder);
-    }
-    else if($("#basic-operation option:selected").html() === 'multiplied by') {
-      var multiplied = parseInt(numone.val()) * parseInt(numtwo.val());
-      $('#basic-answer-alert').html("The Answer is " + multiplied);
-    }
-    else if($("#basic-operation option:selected").html() === 'divided by') {
-      var divided = parseInt(numone.val()) / parseInt(numtwo.val());
-      $('#basic-answer-alert').html("The Answer is " + divided);
-    }
+function tripButton() {
+  var $distance = $('#trip-distance');
+  var $mpg = $('#trip-mpg');
+  var $costgallon = $('#trip-cost');
+  var $speed = $('#trip-speed');
+  var $tripButton = $('#trip-calc');
+$tripButton.click(function() {
+  if(parseInt($speed.val()) < 60) {
+    var tripCost1 = parseFloat($distance.val()) / parseFloat($mpg.val()) * parseFloat($costgallon.val());
+    $('#trip-answer-alert').html("The trip cost is " + tripCost1);
+  } else if(parseInt($speed.val()) >= 60) {
+    var tripCost2 = parseFloat($distance.val()) / ((parseFloat($mpg.val()) - (parseFloat($speed.val()) - 60) * 2)) * parseFloat($costgallon.val());
+    $('#trip-answer-alert').html("The trip cost is " + tripCost2);
   }
-);}
-​
-​
-basicCalc();
+
+
+});
+}
+
+tripButton();
+
+
+function bmiButton() {
+  var $mass = $('#bmi-mass');
+  var $height = $('#bmi-height');
+  var $bmiButton = $('#bmi-calc');
+$bmiButton.click(function() {
+  var bmi = parseFloat($mass.val()) / ((parseFloat($height.val()) * parseFloat($height.val())) * 703);
+  $('#bmi-answer-alert').html("BMI is " + bmi);
+
+});
+}
+
+
+bmiButton();
+
+function loanButton() {
+  var $loan = $('#mortgage-loan');
+  var $apr = $('#mortgage-apr');
+  var $term = $('#mortgage-term');
+  var $loanButton = $('#mortgage-calc');
+$loanButton.click(function() {
+  var mortgage = (parseFloat($loan.val()) * parseFloat($apr.val()) * ((1 + parseFloat($apr.val())) ^ parseFloat($term.val()))) / (((1 + parseFloat($apr.val())) ^ parseFloat($term.val())) - 1);
+  $('#mortgage-answer-alert').html("Mortgage is " + mortgage);
+
+
+});
+}
+
+loanButton();
+
+
+/*The BMI calculator should compute a body mass index, with the formula ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
+The mortgage calculator formula should compute the monthly cost of a loan, with the formula loan * apr * ((1+apr)^term) / (((1+apr)^term) - 1)
+When a user presses a "calculate button", the result should appear in the green div for the appropriate calculator section. The elements where the result should be displayed have an ID in the form #XXX-answer.
 */
