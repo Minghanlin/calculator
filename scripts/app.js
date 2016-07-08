053f48f26932ad9d3dff21b649e0fe85fcf30ffb
Minghan Lin <cd>
2016-07-08 11:37:51 +0800

    finally working

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,86 @@
 
 
+/*var calculator = {
+  numOne: 0,
+  numTwo: 0,
+  operator: "",
+  result: 0,
+  getValue1: function() {
 
-$('#basic-calc').click(clickButton);
+  },
+  getValue2: function() {
 
-function clickButton(){
-  var $numOne = $('basic-num-one').val();
-  var $numTwo = $('basic-num-two').val();
-  if $('#basic-operation' > 'option value='+'') {
-    var $addition = $('addition');
-    $addition = $numOne + $numTwo;
-    $('basic-answer-alert').print($addition);
-  } else if $('#basic-operation' > 'option value='-'') {
-    var $subtraction = $('subtraction');
-    Ssubtraction = $numOne - $numTwo;
-    $('basic-answer-alert').print($subtraction);
-  } else if $('#basic-operation' > 'option value='*'') {
-    var $multiplication = $('multiplication');
-    $multiplication = $numOne * $numTwo;
-    $('basic-answer-alert').print($multiplication);
-  } else $('#basic-operation' > 'option value='/'') {
-    var $division = $('division');
-    $division = $numOne / $numTwo;
-    $('basic-answer-alert').print($division);
   }
+
+};
+
+$(document).ready(function() {
+  $('#basic-calc').click.(function() {
+
+  });
+});
+
+
+
+});*/
+
+
+
+
+function clickButton() {
+  var $numOne = $('#basic-num-1');
+  var $numTwo = $('#basic-num-2');
+  var $clickButton = $('#basic-calc');
+$clickButton.click(function() {
+    if($("#basic-operation option:selected").html() === 'plus') {
+    var sum = parseInt($numOne.val()) + parseInt($numTwo.val());
+    $('#basic-answer-alert').html("The Answer is " + sum);
+  } else if($("#basic-operation option:selected").html() === 'minus') {
+  var subtract = parseInt($numOne.val()) - parseInt($numTwo.val());
+  $('#basic-answer-alert').html("The Answer is " + subtract);
+} else if($("#basic-operation option:selected").html() === 'multiplied by') {
+var multiplied = parseInt($numOne.val()) * parseInt($numTwo.val());
+$('#basic-answer-alert').html("The Answer is " + multiplied);
+} else if($("#basic-operation option:selected").html() === 'divided by') {
+var divided = parseInt($numOne.val()) / parseInt($numTwo.val());
+$('#basic-answer-alert').html("The Answer is " + divided);
+}
+
+});
+}
+
+
+  clickButton();
+
+
+  /*$(document).ready(function() {
+​
+​
+​
+function basicCalc() {
+  var numone = $('#basic-num-1');
+  var numtwo = $('#basic-num-2');
+  var calcnum = $('#basic-calc');
+  calcnum.click(function() {
+    if($("#basic-operation option:selected").html() === 'plus') {
+      var sum = parseInt(numone.val()) + parseInt(numtwo.val());
+      $('#basic-answer-alert').html("The Answer is " + sum);
+    }
+    else if($("#basic-operation option:selected").html() === 'minus') {
+      var remainder = parseInt(numone.val()) - parseInt(numtwo.val());
+      $('#basic-answer-alert').html("The Answer is " + remainder);
+    }
+    else if($("#basic-operation option:selected").html() === 'multiplied by') {
+      var multiplied = parseInt(numone.val()) * parseInt(numtwo.val());
+      $('#basic-answer-alert').html("The Answer is " + multiplied);
+    }
+    else if($("#basic-operation option:selected").html() === 'divided by') {
+      var divided = parseInt(numone.val()) / parseInt(numtwo.val());
+      $('#basic-answer-alert').html("The Answer is " + divided);
+    }
+  }
+);}
+​
+​
+basicCalc();
+*/
